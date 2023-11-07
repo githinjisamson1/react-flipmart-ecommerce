91d1d80bb262d4070f78afb1d6cde179321028a4
Samson Githinji <githinjisamson148@gmail.com>
2023-11-07 15:23:59 +0300

    Setup Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Products from "./pages/productsPage/Products";
 import ShoppingCart from "./pages/shoppingCartPage/ShoppingCart";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div className="App">
       {/* configure routes */}
+      <Navbar/>
       <Routes>
         <Route path="/products" element={<Products />} />
         <Route path="/shoppingCart" element={<ShoppingCart />} />
