694c2d5036b39a976ebf22b110979b87c808e1f8
Samson Githinji <githinjisamson148@gmail.com>
2023-11-08 01:05:50 +0300

    Wrap App in ProductsProvider

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 import { BrowserRouter } from "react-router-dom";
+import { ProductsProvider } from "./context/productsContext";
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <ProductsProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ProductsProvider>
 );
