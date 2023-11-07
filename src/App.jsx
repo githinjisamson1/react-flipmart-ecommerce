082b06a033ad85cd7c1c26b07d2523e33d480e1e
Samson Githinji <githinjisamson148@gmail.com>
2023-11-07 15:15:20 +0300

    Configure routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Products from "./pages/productsPage/Products";
+import ShoppingCart from "./pages/shoppingCartPage/ShoppingCart";
 
 const App = () => {
   return (
-    <div className="App">App</div>
-  )
-}
+    <div className="App">
+      {/* configure routes */}
+      <Routes>
+        <Route path="/products" element={<Products />} />
+        <Route path="/shoppingCart" element={<ShoppingCart />} />
+      </Routes>
+    </div>
+  );
+};
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
