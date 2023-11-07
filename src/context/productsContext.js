a08165cdf92fec6e7f174b0c6d98d20d5622cf40
Samson Githinji <githinjisamson148@gmail.com>
2023-11-08 00:40:20 +0300

    Setup productsReducer

--- src/context/productsContext.js
+++ src/context/productsContext.js
@@ -1,10 +1,59 @@
-import React, { useContext } from "react";
+import React, { useContext, useReducer } from "react";
 
+// ProductsContext
 const ProductsContext = React.createContext();
+
+// Provider
 export const ProductsProvider = ({ children }) => {
-  return <ProductsContext.Provider>{children}</ProductsContext.Provider>;
+  // initialProductsState
+  const initialProductsState = {
+    loading: false,
+    products: [],
+    error: "",
+  };
+
+  // reducer
+  const productsReducer = (state, action) => {
+    switch (action.type) {
+      case "FETCH_REQUEST":
+        return {
+          ...state,
+          loading: true,
+        };
+      case "FETCH_SUCCESS":
+        return {
+          ...state,
+          loading: false,
+          products: action.payload,
+          error: "",
+        };
+      case "FETCH_ERROR":
+        return {
+          ...state,
+          loading: false,
+          products: [],
+          error: action.payload,
+        };
+
+      default:
+        return state;
+    }
+  };
+
+  // useReducer
+  const [productsState, dispatchForProducts] = useReducer(
+    productsReducer,
+    initialProductsState
+  );
+
+  return (
+    <ProductsContext.Provider value={{ productsState, dispatchForProducts }}>
+      {children}
+    </ProductsContext.Provider>
+  );
 };
 
+// useGlobalProductsContext
 export const useGlobalProductsContext = () => {
   return useContext(ProductsContext);
 };
