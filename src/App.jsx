8164fc82ce4858da4485b006157b32f41134cf1e
Samson Githinji <githinjisamson148@gmail.com>
2023-11-08 14:53:38 +0300

    Configure dynamic products/:productId route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Products from "./pages/productsPage/Products";
 import ShoppingCart from "./pages/shoppingCartPage/ShoppingCart";
 import Navbar from "./components/navbar/Navbar";
+import Details from "./pages/detailsPage/Details";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       {/* configure routes */}
       <Routes>
         <Route index path="/products" element={<Products />} />
+        <Route path="products/:productId" element={<Details />} />
         <Route path="/shoppingCart" element={<ShoppingCart />} />
       </Routes>
     </div>
