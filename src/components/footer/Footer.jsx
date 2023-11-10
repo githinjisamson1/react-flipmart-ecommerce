10756479bff2cd88eef86a593b9a560560f539ff
Samson Githinji <githinjisamson148@gmail.com>
2023-11-10 07:18:43 +0300

    Create Footer component

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./footer.css"
+
+const Footer = () => {
+  return <footer>&copy; Samson Githinji {new Date().getFullYear()}</footer>;
+};
+
+export default Footer;
